bb92f44cbb53241cfd17aaa3287139037d339e67
Ahmad Mustafa <support@skipcash.com>
2024-05-01 20:45:17 +0300

    latest

--- src/web.ts
+++ src/web.ts
@@ -4,6 +4,8 @@ import type { ScApplePayPlugin } from './definitions';
 
 export class ScApplePayWeb extends WebPlugin implements ScApplePayPlugin {
   loadSCPGW(options: { payUrl: string, paymentTitle: string, returnURL: string }): void {
-
+    console.log(options.payUrl);
+    console.log(options.paymentTitle);
+    console.log(options.returnURL);
   }
 }
